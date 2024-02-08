ba7a6366352e407a3029b92742671e404d04e6ef
zchen125 <zchen125@stevens.edu>
2024-02-08 14:17:35 -0500

    add profile details and update grid view to pictures only

--- art-social/src/pages/ProfileGrid.js
+++ art-social/src/pages/ProfileGrid.js
@@ -3,12 +3,12 @@ import profileStyles from './Styles/profileStyles.module.css';
 
 
 const ProfileGrid = ({posts, handleDeletePost}) => {
-
+  const postsWithImages = posts.filter(post => post.post.embed && post.post.embed.images && post.post.embed.images[0]);
   return (
     <div>
       <h1>User Posts - Grid View</h1>
         <div className={profileStyles.gridContainer}>
-          {posts.map((post) => (
+          {postsWithImages.map((post) => (
             <div key={post.post.cid}>
                 {post.post.embed && post.post.embed.images && post.post.embed.images[0] && (
                     <div className={profileStyles.imageThumbnailBox}>
