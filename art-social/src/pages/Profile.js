a57e154313a06ed678cce47299aa4de9d8e2167e
zchen125 <zchen125@stevens.edu>
2024-04-03 20:30:21 -0400

    refactorings

--- art-social/src/pages/Profile.js
+++ art-social/src/pages/Profile.js
@@ -1,32 +1,28 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import { getMyHandle } from "../lib/bsky.ts";
 import { useNavigate } from "react-router-dom";
 
-// until we're able to store the handle across pages, this is the only way I can think of redirecting the user to their profile
 const Profile = () => {
   const [userHandle, setUserHandle] = useState("");
   const navigate = useNavigate();
 
-  fetchHandle();
-
-  async function fetchHandle() {
-    try {
-      setUserHandle(await getMyHandle());
-      navigate(`/profile/${userHandle}`);
-    } catch (error) {
-      console.error("Error fetching user handle:", error);
+  useEffect(() => {
+    async function fetchHandle() {
+      try {
+        const handle = await getMyHandle();
+        setUserHandle(handle);
+        navigate(`/profile/${handle}`);
+      } catch (error) {
+        console.error("Error fetching user handle:", error);
+      }
     }
-  }
 
-
-  const redirectToUserProfile = () => {
-    navigate(`/profile/${userHandle}`);
-  };
+    fetchHandle();
+  }, []);
 
   return (
     <div>
-      {/* Your Profile component content goes here */}
-      <button onClick={redirectToUserProfile}>View Profile</button>
+      <p>Redirecting...</p>
     </div>
   );
 };
--- art-social/src/pages/components/ProfileTimeline.js
+++ art-social/src/pages/components/ProfileTimeline.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Post from './Post';
 
-const defaultAvatar =
-  "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png";
-
 const ProfileTimeline = ({ posts, handleDeletePost, userHandle }) => {
 
   return (
