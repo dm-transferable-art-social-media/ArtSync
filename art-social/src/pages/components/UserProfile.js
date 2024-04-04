d2af47b8f856c35c8864d1a1704bc7791ab92457
Ashna Razdan <65045279+ashnarazdan123@users.noreply.github.com>
2024-04-03 21:35:56 -0400

    fixed errors with profile

--- art-social/src/pages/components/UserProfile.js
+++ art-social/src/pages/components/UserProfile.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { tryResumeSession, getAuthorFeed, deletePost, getProfile, getMyHandle, getFollowers, getFollows } from "../../lib/bsky.ts";
+import {
+  tryResumeSession,
+  getAuthorFeed,
+  deletePost,
+  getProfile,
+  getMyHandle,
+  getFollowers,
+  getFollows,
+} from "../../lib/bsky.ts";
 import ProfileTimeline from "./ProfileTimeline.js";
 import ProfileGrid from "./ProfileGrid";
 import profileStyles from "../styles/profileStyles.module.css";
-import followsStyles from "../styles/followsPopup.module.css"
+import followsStyles from "../styles/followsPopup.module.css";
 import CreatePost from "./CreatePost.js";
 import { defaultAvatar, defaultBanner } from "../assets/defaultImages.js";
 import Follows from "./Follows.js";
+import FetchPostsFromDatabase from "./FetchPostsFromDatabase.js";
 
 const UserProfile = () => {
   const [view, setView] = useState("grid");
@@ -105,7 +114,7 @@ const UserProfile = () => {
 
   const toggleUpload = () => {
     setShowUpload(!showUpload);
-  }
+  };
 
   return (
     <div>
@@ -133,7 +142,6 @@ const UserProfile = () => {
           </div>
           <div className={profileStyles.profileHandle}>@{handle}</div>
 
-
           <div style={{ display: "flex" }}>
             <div className={profileStyles.profileText}>
               <button className="third-button" onClick={toggleFollowers}>
@@ -149,25 +157,34 @@ const UserProfile = () => {
               </div>
             </div>
 
-            <div style={{ marginLeft: "10px", paddingTop: "5px" }} className={profileStyles.profileText}>
+            <div
+              style={{ marginLeft: "10px", paddingTop: "5px" }}
+              className={profileStyles.profileText}
+            >
               {profile.postsCount} Posts
             </div>
           </div>
         </div>
         <p></p>
         <div className="button-container">
-          <button className="secondary-button" onClick={() => handleSwitchView("grid")}>
+          <button
+            className="secondary-button"
+            onClick={() => handleSwitchView("grid")}
+          >
             Switch to Grid View
           </button>
-          <button className="secondary-button" onClick={() => handleSwitchView("timeline")}>
+          <button
+            className="secondary-button"
+            onClick={() => handleSwitchView("timeline")}
+          >
             Switch to Timeline
           </button>
-
 
           <button className="primary-button" onClick={toggleUpload}>
             Create New Post
           </button>
 
+          <FetchPostsFromDatabase />
         </div>
         {loading ? (
           <p>Loading...</p>
@@ -191,7 +208,12 @@ const UserProfile = () => {
         <div className={followsStyles.popupContainer}>
           <div className={followsStyles.popupHeader}>
             <h2>Following</h2>
-            <button className={followsStyles.closeButton} onClick={toggleFollows}>x</button>
+            <button
+              className={followsStyles.closeButton}
+              onClick={toggleFollows}
+            >
+              x
+            </button>
           </div>
           <Follows type={"follows"} followers={follows} />
         </div>
@@ -200,7 +222,12 @@ const UserProfile = () => {
         <div className={followsStyles.popupContainer}>
           <div className={followsStyles.popupHeader}>
             <h2>Followers</h2>
-            <button className={followsStyles.closeButton} onClick={toggleFollowers}>x</button>
+            <button
+              className={followsStyles.closeButton}
+              onClick={toggleFollowers}
+            >
+              x
+            </button>
           </div>
           <Follows type={"followers"} followers={followers} />
         </div>
@@ -210,13 +237,16 @@ const UserProfile = () => {
         <div className={followsStyles.popupContainer}>
           <div className={followsStyles.popupHeader}>
             <h2>Create Post</h2>
-            <button className={followsStyles.closeButton} onClick={toggleUpload}>x</button>
+            <button
+              className={followsStyles.closeButton}
+              onClick={toggleUpload}
+            >
+              x
+            </button>
           </div>
           <CreatePost />
         </div>
       )}
-
-
     </div>
   );
 };
